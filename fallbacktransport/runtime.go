c91a2a25d3cd142aed3ad27fd4396693ab69c2f2
fengyinsolo <1318978787@qq.com>
2026-08-27 18:46:25 +0800

    bug: 44-notification-center【30】-016

---
+++ fallbacktransport/runtime.go
@@ -0,0 +1,23 @@
+package fallbacktransport
+
+import (
+    "errors"
+    "fmt"
+)
+
+var ErrRejected = errors.New("request rejected")
+var ErrTemporary = errors.New("temporary unavailable")
+
+type Gateway struct { outcomes []error; calls int }
+
+func NewGateway(outcomes ...error) *Gateway { return &Gateway{outcomes: outcomes} }
+
+func (g *Gateway) Send(key string) error {
+    g.calls++
+    index := g.calls - 1
+    if index >= len(g.outcomes) { return nil }
+    if err := g.outcomes[index]; err != nil { return fmt.Errorf("send %s: %v", key, err) }
+    return nil
+}
+
+func (g *Gateway) Calls() int { return g.calls }
